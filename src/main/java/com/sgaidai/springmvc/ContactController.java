ede82705131b600494e35e3ae135c7e031b54ece
gaidai <q-daddy@mail.ru>
2017-09-19 04:31:07 +0300

    created pages for updating and creating contacts

--- src/main/java/com/sgaidai/springmvc/ContactController.java
+++ src/main/java/com/sgaidai/springmvc/ContactController.java
@@ -1,13 +1,23 @@
 package com.sgaidai.springmvc;
 
+
 import java.util.List;
+import java.util.Locale;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 
 @RequestMapping("/contacts")
@@ -16,7 +26,8 @@ public class ContactController {
     private final Logger logger = LoggerFactory.getLogger(ContactController.class);
 
     private ContactService contactService;    
-
+    private MessageSource messageSource;
+    
     @RequestMapping(method = RequestMethod.GET)
     public String list(Model uiModel) {
         logger.info("Listing contacts");
@@ -29,9 +40,74 @@ public class ContactController {
         System.out.println("com.sgaidai.springmvc.ContactController.list()" + contacts);
         return "contacts/list";
     }
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public String show(@PathVariable("id") Long id, Model uiModel){
+        Contact contact = contactService.findById(id);
+        uiModel.addAttribute("contact", contact);
+        return "contacts/show";
+    }
+    
+    @RequestMapping(value = "/{id}", params = "form", method = RequestMethod.POST)
+    public String update(@Valid Contact contact, BindingResult bindingResult, Model uiModel,
+                         HttpServletRequest httpServletRequest, RedirectAttributes redirectAttributes,
+                         Locale locale) {
+        logger.info("Updating contact");
+        if (bindingResult.hasErrors()) {
+            uiModel.addAttribute("message", new Message("error",
+                    messageSource.getMessage("contact_save_fail", new Object[]{}, locale)));
+            uiModel.addAttribute("contact", contact); 
+            return "contacts/update";
+        }
+        uiModel.asMap().clear();
+        redirectAttributes.addFlashAttribute("message", new Message("success",
+                messageSource.getMessage("contact_save_success", new Object[]{}, locale)));
+        contactService.save(contact);
+        return "redirect:/contacts/" + UrlUtil.encodeUrlPathSegment(contact.getId().toString(), httpServletRequest);
+        
+    }
+
+    @RequestMapping(value = "/{id}", params = "form", method = RequestMethod.GET)
+    public String updateForm(@PathVariable("id") Long id, Model uiModel) {
+        uiModel.addAttribute("contact", contactService.findById(id));
+        return "contacts/update";
+    }
+    
+    
+    @RequestMapping(params = "form", method = RequestMethod.POST)
+    public String create(Contact contact, BindingResult bindingResult,
+        Model uiModel, HttpServletRequest httpServletRequest,
+        RedirectAttributes redirectAttributes, Locale locale) {
+        logger.info("Creating contact");
+        if (bindingResult.hasErrors()) {
+            uiModel.addAttribute("message", new Message("error",
+            messageSource.getMessage("contact_save_fail",
+            new Object[] {}, locale)));
+            uiModel.addAttribute("contact", contact);
+            return "contacts/create";
+        }
+        uiModel.asMap().clear();
+        redirectAttributes.addFlashAttribute("message",  new Message("success",
+            messageSource.getMessage("contact_save_success", new Object[] {}, locale)));
+        logger.info("Contact id: " + contact.getId());
+        contactService.save(contact);
+        return "redirect:/contacts/";
+    }
+    
+    @RequestMapping(params = "form", method = RequestMethod.GET)
+    public String createForm(Model uiModel) {
+        Contact contact = new Contact();
+        uiModel.addAttribute("contact", contact);
+
+        return "contacts/create";
+    }
+    
     @Autowired
     public void setContactService(ContactService contactService){
             this.contactService = contactService;
+    }
+    @Autowired
+    public void setMessageSource(MessageSource messageSource) {
+        this.messageSource = messageSource;
     }
 }
    
